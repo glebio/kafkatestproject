859f5c120753a310ac8b3fb194baf96ee43bce43
Glebio <gmatsiash@gmail.com>
2017-08-08 11:34:27 +0300

    kafka test project

--- src/test/java/com/epam/SimpleKafkaConsumerTest.java
+++ src/test/java/com/epam/SimpleKafkaConsumerTest.java
@@ -1,15 +1,19 @@
 package com.epam;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.MockConsumer;
 import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.record.TimestampType;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class SimpleKafkaConsumerTest {
 
@@ -22,29 +26,41 @@ public class SimpleKafkaConsumerTest {
         consumer = new MockConsumer<String, String>(OffsetResetStrategy.EARLIEST);
     }
 
-    @Test
-    public void testConsumer() throws IOException {
-        SimpleKafkaConsumer myTestConsumer = new SimpleKafkaConsumer(TOPIC);
-        myTestConsumer.myConsumer = consumer;
 
-        consumer.assign(Arrays.asList(new TopicPartition("my_topic", 0)));
+    @Test
+    public void testSimpleConsumer() {
+
+        consumer.assign(Arrays.asList(new TopicPartition(TOPIC, 0)));
 
         HashMap<TopicPartition, Long> beginningOffsets = new HashMap<>();
-        beginningOffsets.put(new TopicPartition("my_topic", 0), 0L);
+        beginningOffsets.put(new TopicPartition(TOPIC, 0), 0L);
         consumer.updateBeginningOffsets(beginningOffsets);
 
-        consumer.addRecord(new ConsumerRecord<String, String>("my_topic", 0,
-                0L, "0", "message 0"));
-        consumer.addRecord(new ConsumerRecord<String, String>("my_topic", 0,
-                1L, "1", "message 1"));
-        consumer.addRecord(new ConsumerRecord<String, String>("my_topic", 0,
-                2L, "2", "message 1"));
-        consumer.addRecord(new ConsumerRecord<String, String>("my_topic", 0,
-                3L, "3", "message 3"));
-        consumer.addRecord(new ConsumerRecord<String, String>("my_topic", 0,
-                4L, "4", "message 4"));
+        ConsumerRecord<String, String> rec1 = new ConsumerRecord<String, String>(TOPIC, 0,
+                0L, "0", "message 0");
+        ConsumerRecord<String, String> rec2 = new ConsumerRecord<String, String>(TOPIC, 0,
+                1L, "1", "message 1");
+        ConsumerRecord<String, String> rec3 = new ConsumerRecord<String, String>(TOPIC, 0,
+                2L, "2", "message 2");
+        ConsumerRecord<String, String> rec4 = new ConsumerRecord<String, String>(TOPIC, 0,
+                3L, "3", "message 3");
 
-        myTestConsumer.consumeData(5_000);
+        consumer.addRecord(rec1);
+        consumer.addRecord(rec2);
+        consumer.addRecord(rec3);
+        consumer.addRecord(rec4);
 
+
+        ConsumerRecords<String, String> recs = consumer.poll(1);
+        Iterator<ConsumerRecord<String, String>> iter = recs.iterator();
+
+        Assert.assertEquals(iter.next(), rec1);
+        Assert.assertEquals(iter.next(), rec2);
+        Assert.assertEquals(iter.next(), rec3);
+        Assert.assertEquals(iter.next(), rec4);
+        Assert.assertFalse(iter.hasNext());
+
+        //method "position" uses get the offset of the next record that will be fetched
+        Assert.assertEquals(consumer.position(new TopicPartition(TOPIC, 0)), 4L);
     }
 }
